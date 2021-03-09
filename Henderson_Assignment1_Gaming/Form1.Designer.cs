217a21dc4c6fc410d48bd1a9d732c0c78144ce55
serena-henderson <79954513+serena-henderson@users.noreply.github.com>
2021-03-09 02:37:52 -0800

    Add files via upload
    Uploaded finished project

---
+++ Henderson_Assignment1_Gaming/Form1.Designer.cs
@@ -0,0 +1,311 @@
+﻿namespace Henderson_Assignment1_Gaming
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.gamerTag = new System.Windows.Forms.TextBox();
+            this.console = new System.Windows.Forms.TextBox();
+            this.endsWith = new System.Windows.Forms.TextBox();
+            this.startsWith = new System.Windows.Forms.TextBox();
+            this.search = new System.Windows.Forms.TextBox();
+            this.hoursPlayed = new System.Windows.Forms.TextBox();
+            this.output = new System.Windows.Forms.TextBox();
+            this.gamerSub = new System.Windows.Forms.Button();
+            this.conHourSub = new System.Windows.Forms.Button();
+            this.searchSub = new System.Windows.Forms.Button();
+            this.startsSub = new System.Windows.Forms.Button();
+            this.endsSub = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(241, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(247, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Gaming Database";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(43, 114);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Gamer Tag";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(43, 222);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Console";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(43, 278);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(135, 24);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Hours Played";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(43, 374);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(114, 24);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Search For";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(43, 431);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(108, 24);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Starts With";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(43, 487);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(105, 24);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Ends With";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(43, 578);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(150, 24);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Search Results";
+            // 
+            // gamerTag
+            // 
+            this.gamerTag.Location = new System.Drawing.Point(199, 113);
+            this.gamerTag.Margin = new System.Windows.Forms.Padding(4);
+            this.gamerTag.Name = "gamerTag";
+            this.gamerTag.Size = new System.Drawing.Size(341, 22);
+            this.gamerTag.TabIndex = 8;
+            // 
+            // console
+            // 
+            this.console.Location = new System.Drawing.Point(199, 222);
+            this.console.Margin = new System.Windows.Forms.Padding(4);
+            this.console.Name = "console";
+            this.console.Size = new System.Drawing.Size(341, 22);
+            this.console.TabIndex = 9;
+            // 
+            // endsWith
+            // 
+            this.endsWith.Location = new System.Drawing.Point(199, 484);
+            this.endsWith.Margin = new System.Windows.Forms.Padding(4);
+            this.endsWith.Name = "endsWith";
+            this.endsWith.Size = new System.Drawing.Size(341, 22);
+            this.endsWith.TabIndex = 10;
+            // 
+            // startsWith
+            // 
+            this.startsWith.Location = new System.Drawing.Point(199, 431);
+            this.startsWith.Margin = new System.Windows.Forms.Padding(4);
+            this.startsWith.Name = "startsWith";
+            this.startsWith.Size = new System.Drawing.Size(341, 22);
+            this.startsWith.TabIndex = 11;
+            // 
+            // search
+            // 
+            this.search.Location = new System.Drawing.Point(199, 370);
+            this.search.Margin = new System.Windows.Forms.Padding(4);
+            this.search.Name = "search";
+            this.search.Size = new System.Drawing.Size(341, 22);
+            this.search.TabIndex = 12;
+            // 
+            // hoursPlayed
+            // 
+            this.hoursPlayed.Location = new System.Drawing.Point(199, 278);
+            this.hoursPlayed.Margin = new System.Windows.Forms.Padding(4);
+            this.hoursPlayed.Name = "hoursPlayed";
+            this.hoursPlayed.Size = new System.Drawing.Size(341, 22);
+            this.hoursPlayed.TabIndex = 13;
+            // 
+            // output
+            // 
+            this.output.Location = new System.Drawing.Point(71, 615);
+            this.output.Margin = new System.Windows.Forms.Padding(4);
+            this.output.Multiline = true;
+            this.output.Name = "output";
+            this.output.ReadOnly = true;
+            this.output.Size = new System.Drawing.Size(579, 186);
+            this.output.TabIndex = 14;
+            // 
+            // gamerSub
+            // 
+            this.gamerSub.Location = new System.Drawing.Point(551, 111);
+            this.gamerSub.Margin = new System.Windows.Forms.Padding(4);
+            this.gamerSub.Name = "gamerSub";
+            this.gamerSub.Size = new System.Drawing.Size(100, 28);
+            this.gamerSub.TabIndex = 15;
+            this.gamerSub.Text = "Submit";
+            this.gamerSub.UseVisualStyleBackColor = true;
+            this.gamerSub.Click += new System.EventHandler(this.gamerSub_Click);
+            // 
+            // conHourSub
+            // 
+            this.conHourSub.Location = new System.Drawing.Point(551, 276);
+            this.conHourSub.Margin = new System.Windows.Forms.Padding(4);
+            this.conHourSub.Name = "conHourSub";
+            this.conHourSub.Size = new System.Drawing.Size(100, 28);
+            this.conHourSub.TabIndex = 16;
+            this.conHourSub.Text = "Submit";
+            this.conHourSub.UseVisualStyleBackColor = true;
+            this.conHourSub.Click += new System.EventHandler(this.conHourSub_Click);
+            // 
+            // searchSub
+            // 
+            this.searchSub.Location = new System.Drawing.Point(551, 368);
+            this.searchSub.Margin = new System.Windows.Forms.Padding(4);
+            this.searchSub.Name = "searchSub";
+            this.searchSub.Size = new System.Drawing.Size(100, 28);
+            this.searchSub.TabIndex = 17;
+            this.searchSub.Text = "Submit";
+            this.searchSub.UseVisualStyleBackColor = true;
+            this.searchSub.Click += new System.EventHandler(this.searchSub_Click);
+            // 
+            // startsSub
+            // 
+            this.startsSub.Location = new System.Drawing.Point(551, 428);
+            this.startsSub.Margin = new System.Windows.Forms.Padding(4);
+            this.startsSub.Name = "startsSub";
+            this.startsSub.Size = new System.Drawing.Size(100, 28);
+            this.startsSub.TabIndex = 18;
+            this.startsSub.Text = "Submit";
+            this.startsSub.UseVisualStyleBackColor = true;
+            this.startsSub.Click += new System.EventHandler(this.startsSub_Click);
+            // 
+            // endsSub
+            // 
+            this.endsSub.Location = new System.Drawing.Point(551, 481);
+            this.endsSub.Margin = new System.Windows.Forms.Padding(4);
+            this.endsSub.Name = "endsSub";
+            this.endsSub.Size = new System.Drawing.Size(100, 28);
+            this.endsSub.TabIndex = 19;
+            this.endsSub.Text = "Submit";
+            this.endsSub.UseVisualStyleBackColor = true;
+            this.endsSub.Click += new System.EventHandler(this.endsSub_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(744, 817);
+            this.Controls.Add(this.endsSub);
+            this.Controls.Add(this.startsSub);
+            this.Controls.Add(this.searchSub);
+            this.Controls.Add(this.conHourSub);
+            this.Controls.Add(this.gamerSub);
+            this.Controls.Add(this.output);
+            this.Controls.Add(this.hoursPlayed);
+            this.Controls.Add(this.search);
+            this.Controls.Add(this.startsWith);
+            this.Controls.Add(this.endsWith);
+            this.Controls.Add(this.console);
+            this.Controls.Add(this.gamerTag);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox gamerTag;
+        private System.Windows.Forms.TextBox console;
+        private System.Windows.Forms.TextBox endsWith;
+        private System.Windows.Forms.TextBox startsWith;
+        private System.Windows.Forms.TextBox search;
+        private System.Windows.Forms.TextBox hoursPlayed;
+        private System.Windows.Forms.TextBox output;
+        private System.Windows.Forms.Button gamerSub;
+        private System.Windows.Forms.Button conHourSub;
+        private System.Windows.Forms.Button searchSub;
+        private System.Windows.Forms.Button startsSub;
+        private System.Windows.Forms.Button endsSub;
+    }
+}
+
---
+++ Henderson_Assignment1_Gaming/Form1.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Henderson_Assignment1_Gaming
+{
+
+    public partial class Form1 : Form
+    {
+        //3-8-21 SH NEW 6L: Create class for the gamer
+        class Gamer
+        {
+            public int GamerID { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+        }
+
+        //3-8-21 SH NEW 7L: Create class info for the console and hours
+        class GameInfo
+        {
+            public int InfoID { get; set; }
+            public string Console { get; set; }
+            public string Hours { get; set; }
+            public int GamerID { get; set; }
+        }
+
+        //3-8-21 SH NEW : create 2 lists
+        private List<Gamer> gamers = new List<Gamer>();
+        private List<GameInfo> info = new List<GameInfo>();
+
+        private int gamerID, infoID;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void gamerSub_Click(object sender, EventArgs e)
+        {
+            //3-8-21 SH NEW 5L: Error Handling for tag
+            if (string.IsNullOrEmpty(gamerTag.Text.Trim()))
+            {
+                output.Text = "Please enter the gamer tag.";
+                return;
+            }
+
+            //3-8-21 SH NEW 2L: Separates between first and last names
+            gamerTag.Text.Trim();
+            string[] tags = gamerTag.Text.Split(' ');
+
+            Gamer gamer = new Gamer();
+
+            //3-8-21 SH NEW 1L: Each tag gets a different ID
+            gamer.GamerID = gamerID++;
+
+            //3-8-21 SH NEW 1L: first name is inserted
+            gamer.FirstName = tags[0];
+
+            //3-8-21 SH NEW 8L: tracks the Last name
+            if (tags.Count() == 1)
+            {
+                gamer.LastName = string.Empty;
+            }
+            else
+            {
+                gamer.LastName = tags[tags.Count() - 1];
+            }
+
+            //3-8-21 SH NEW 3L: output
+            gamers.Add(gamer);
+
+            output.Text = "The saved tag is " + gamerTag.Text + "...";
+
+        }
+
+        private void conHourSub_Click(object sender, EventArgs e)
+        {
+            //3-8-21 SH NEW 5L: Error Handling for tag
+            if (gamers.Count == 0 || string.IsNullOrEmpty(gamers[gamers.Count - 1].FirstName))
+            {
+                output.Text = "Please enter the gamer tag!";
+                console.Clear();
+                hoursPlayed.Clear();
+                return;
+            }
+
+            //3-8-21 SH NEW 5L: Error Handling for console
+            if (string.IsNullOrEmpty(console.Text.Trim()))
+            {
+                output.Text = "Please enter the console name.";
+                return;
+            }
+
+            //3-8-21 SH NEW 5L: Error Handling for hours
+            if (string.IsNullOrEmpty(hoursPlayed.Text.Trim()))
+            {
+                output.Text = "Please enter the hours played.";
+                return;
+            }
+
+            //3-8-21 SH NEW 5L: Error Handling for hours
+            decimal spent = 0;
+            if (decimal.TryParse(hoursPlayed.Text.Trim(), out spent) == false)
+            {
+                output.Text = "Please enter a number.";
+                hoursPlayed.Clear();
+                return;
+            }
+
+            GameInfo gameInfo = new GameInfo();
+
+            //3-8-21 SH NEW 5L: assigning ID
+            gameInfo.InfoID = infoID++;
+            gameInfo.Console = console.Text;
+            gameInfo.Hours = hoursPlayed.Text;
+            gameInfo.GamerID = gamers.Last().GamerID;
+
+            //3-8-21 SH NEW 5L: output
+            info.Add(gameInfo);
+
+            output.Text = "The console is " + console.Text + " and the hours played is " + hoursPlayed.Text + "...";
+
+            //3-8-21 SH NEW 5L: clear all fields
+            gamerTag.Clear();
+            console.Clear();
+            hoursPlayed.Clear();
+
+        }
+
+        private void searchSub_Click(object sender, EventArgs e)
+        {
+            string searchFull = search.Text.Trim();
+
+            //3-8-21 SH NEW 14L: Combined the lists to search everything
+            var query = info.Join(
+                gamers,
+                info => info.InfoID,
+                gamer => gamer.GamerID,
+                (info, gamer) => new
+                {
+                    FirstName = gamer.FirstName,
+                    LastName = gamer.LastName,
+                    Console = info.Console,
+                    Hours = info.Hours,
+                }).Where(s => s.FirstName == searchFull ||
+                              s.LastName == searchFull ||
+                              s.Console == searchFull ||
+                              s.Hours == searchFull);
+
+            output.Clear();
+
+            //3-8-21 SH NEW 6L: output
+            foreach (var result in query)
+            {
+                output.AppendText("Name: " + result.FirstName +
+                    result.LastName + "  -  Console: " + result.Console +
+                    "  -  Hours: " + int.Parse(result.Hours) + Environment.NewLine);
+            }
+
+            search.Clear();
+        }
+
+        private void startsSub_Click(object sender, EventArgs e)
+        {
+            string searchStarts = startsWith.Text.Trim();
+
+            //3-8-21 SH NEW 14L: Combined the lists to search everything
+            var query = info.Join(
+                gamers,
+                info => info.InfoID,
+                gamer => gamer.GamerID,
+                (info, gamer) => new
+                {
+                    FirstName = gamer.FirstName,
+                    LastName = gamer.LastName,
+                    Console = info.Console,
+                    Hours = info.Hours,
+                }).Where(s => s.FirstName.StartsWith(searchStarts) == true ||
+                              s.LastName.StartsWith(searchStarts) == true ||
+                              s.Console.StartsWith(searchStarts) == true ||
+                              s.Hours.StartsWith(searchStarts) == true);
+
+            output.Clear();
+
+            //3-8-21 SH NEW 6L: output
+            foreach (var result in query)
+            {
+                output.AppendText("Name: " + result.FirstName +
+                    result.LastName + "  -  Console: " + result.Console +
+                    "  -  Hours: " + int.Parse(result.Hours) + Environment.NewLine);
+            }
+
+            startsWith.Clear();
+        }
+
+        private void endsSub_Click(object sender, EventArgs e)
+        {
+            string searchEnds = endsWith.Text.Trim();
+
+            //3-8-21 SH NEW 14L: Combined the lists to search everything
+            var query = info.Join(
+                gamers,
+                info => info.InfoID,
+                gamer => gamer.GamerID,
+                (info, gamer) => new
+                {
+                    FirstName = gamer.FirstName,
+                    LastName = gamer.LastName,
+                    Console = info.Console,
+                    Hours = info.Hours,
+                }).Where(s => s.FirstName.EndsWith(searchEnds) == true ||
+                              s.LastName.EndsWith(searchEnds) == true ||
+                              s.Console.EndsWith(searchEnds) == true ||
+                              s.Hours.EndsWith(searchEnds) == true);
+
+            output.Clear();
+
+            //3-8-21 SH NEW 6L: output
+            foreach (var result in query)
+            {
+                output.AppendText("Name: " + result.FirstName +
+                    result.LastName + "  -  Console: " + result.Console +
+                    "  -  Hours: " + int.Parse(result.Hours) + Environment.NewLine);
+            }
+
+            endsWith.Clear();
+        }
+    }
+}
